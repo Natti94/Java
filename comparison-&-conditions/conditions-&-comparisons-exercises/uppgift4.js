8c49543b8f172e103762a8a07acd56db66a1c01e
Natti94 <natnael_94@hotmail.com>
2024-10-13 13:52:34 +0200

    update

--- comparison-&-conditions/conditions-&-comparisons-exercises/uppgift4.js
+++ comparison-&-conditions/conditions-&-comparisons-exercises/uppgift4.js
@@ -14,24 +14,51 @@ Nedan finner ni en startkod för hur man gör ett slumpvalt nummer.
 */ 
 
 //Tar ett slumpvalt värde mellan 0 och 10
+
 var nummer = Math.random();
 console.log(nummer);
 nummer = nummer * 10;
 console.log(nummer);
 nummer = Math.round(nummer);
 console.log(nummer);
+
+// --------------------------------------------------------------------------------------------------------------//
+
 /* FORTSÄTT MED EGEN KOD HÄR */
 
+high = "number was high"
 
+var restart = function() 
 
-var guess = parseFloat(prompt("guess the number between 1-10:"));
+{ 
+
+var guess = parseFloat(prompt("Guess the number between 1-10:"));
 if (guess == nummer)
 {
-alert("you won!")
-
+alert("YOU WON! " + "the number was: " + (nummer))
+return;
 }
+
+var guessTwo = parseFloat(prompt("Two more chances, guess the number between 1-10:"));
+if (guessTwo == nummer)
+{
+alert("YOU WON! " + "the number was: " + (nummer))
+return;
+}
+
+var guessThree = parseFloat(prompt("One more chance, guess the number between 1-10:"));
+if (guessThree == nummer)
+{
+alert("YOU WON! " + "the number was: " + (nummer))
+return;
+}
+
 else (guess ==! nummer)
 {
-alert("you lost!")
+alert("dumb fk u lost!")
+return;
 }
 
+}
+
+restart();
